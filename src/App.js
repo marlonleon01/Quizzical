8c78f897131e41359c1cc6ff6b01c4316f75b24b
Marlon Leon <ghostveene@gmail.com>
2023-03-14 22:19:41 -0400

    added homescreen and quiz components and wrapped in main elem

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
+import Homescreen from "./Components/Homescreen/Homescreen"
+import Quiz from "./Components/Quiz/Quiz"
+
 export default function App() {
-  
+    <main>
+        <Homescreen />
+        <Quiz />
+    </main>
 }
 
