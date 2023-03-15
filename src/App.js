7a20b903662b63a91f840536dbfd94194fc5e435
Marlon Leon <ghostveene@gmail.com>
2023-03-15 10:14:33 -0400

    starting test with state change

--- src/App.js
+++ src/App.js
@@ -6,29 +6,24 @@ import blob2 from "./images/blob 5.png"
 
 export function App() {
     //make start button do something
+    const [startQuiz, setStartQuiz] = useState(false)
 
     function getQuiz() {
-        fetch("https://opentdb.com/api.php?amount=5&type=multiple")
-            .then(res => res.json())
-            .then(data => {
-                console.log(data.results)
-                return (
-                    <Quiz 
-                        question={data.results[0].question}
-                        incorrectAnswers={data.results[0].incorrect_answers}
-                        correctAnswer={data.results[0].correct_answer}
-                    />
-                )
-            })
+        setStartQuiz(true)
     }
+    
     //after fetch return quiz with props filled
 
     return (
         <main>
             <img src={blob1} className="blob1"></img>
-            <Homescreen 
-                handleClick={getQuiz}
-            />
+            {
+                startQuiz ? 
+                <Quiz /> : 
+                <Homescreen 
+                    handleClick={getQuiz}
+                />
+            }
             <img src={blob2} className="blob2"></img>
         </main>
     )
