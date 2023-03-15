55491f9ab6e27999571fd554c2cec47dddc5aa7d
Marlon Leon <ghostveene@gmail.com>
2023-03-15 10:58:49 -0400

    retrieving data come back to project when you finish advanced react course

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState} from "react"
+import {useEffect, useState} from "react"
 import { Homescreen } from "./Components/Homescreen/Homescreen"
 import { Quiz } from "./Components/Quiz/Quiz"
 import blob1 from "./images/blobs.png"
@@ -7,11 +7,27 @@ import blob2 from "./images/blob 5.png"
 export function App() {
     //make start button do something
     const [startQuiz, setStartQuiz] = useState(false)
+    const [questions, setQuestions] = useState([])
 
     function getQuiz() {
-        setStartQuiz(true)
+        setStartQuiz(oldStartQuiz => !oldStartQuiz)
     }
     
+    useEffect(() => {
+        if(startQuiz) {
+            fetch("https://opentdb.com/api.php?amount=5")
+                .then(res => res.json())
+                .then(data => setQuestions(data.results.map(data => {
+                    return({
+                        question:data.question,
+                        options:data.incorrect_answer,
+                        selectAnswer: undefined,
+                        correctAnswer: data.correct_answer
+                    })
+                })))
+    
+        }
+    },[startQuiz])
     //after fetch return quiz with props filled
 
     return (
