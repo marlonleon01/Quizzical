a4f615a18f5c25beabc8ad50bc995cc2fe34b87f
Marlon Leon <ghostveene@gmail.com>
2023-03-14 23:32:37 -0400

    inserted blobs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { Homescreen } from "./Components/Homescreen/Homescreen"
 import { Quiz } from "./Components/Quiz/Quiz"
+import blob1 from "./images/blobs.png"
+import blob2 from "./images/blob 5.png"
 
 export function App() {
     return (
         <main>
+            <img src={blob1} className="blob1"></img>
+            <img src={blob2} className="blob2"></img>
             <Homescreen />
             <Quiz />
         </main>
