9f3c0d983703f0e83157eb8a7c670fea910bbc34
Marlon Leon <ghostveene@gmail.com>
2023-03-15 10:16:23 -0400

    removed hardcoded data need to map through results

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -1,51 +1,15 @@
 import "./Quiz.css"
 
-export function Quiz(props) {
+export function Quiz() {
     return (
         <div className="quiz-container">
             <div className="quiz-question-container">
-                <h2 className="quiz-question">How Would one say goodbye in Spanish?</h2>
+                <h2 className="quiz-question"></h2>
                 <div className="quiz-btn-container">
-                    <button className="quiz-answer-btn">Adios</button>
-                    <button className="quiz-answer-btn">Hola</button>
-                    <button className="quiz-answer-btn">Au Revoir</button>
-                    <button className="quiz-answer-btn">Salir</button>
-                </div>
-            </div>
-            <div className="quiz-question-container">
-                <h2 className="quiz-question">How Would one say goodbye in Spanish?</h2>
-               <div className="quiz-btn-container">
-                    <button className="quiz-answer-btn">Adios</button>
-                    <button className="quiz-answer-btn">Hola</button>
-                    <button className="quiz-answer-btn">Au Revoir</button>
-                    <button className="quiz-answer-btn">Salir</button>
-                </div>
-            </div>
-            <div className="quiz-question-container">
-                <h2 className="quiz-question">How Would one say goodbye in Spanish?</h2>
-               <div className="quiz-btn-container">
-                    <button className="quiz-answer-btn">Adios</button>
-                    <button className="quiz-answer-btn">Hola</button>
-                    <button className="quiz-answer-btn">Au Revoir</button>
-                    <button className="quiz-answer-btn">Salir</button>
-                </div>
-            </div>
-            <div className="quiz-question-container">
-                <h2 className="quiz-question">How Would one say goodbye in Spanish?</h2>
-               <div className="quiz-btn-container">
-                    <button className="quiz-answer-btn">Adios</button>
-                    <button className="quiz-answer-btn">Hola</button>
-                    <button className="quiz-answer-btn">Au Revoir</button>
-                    <button className="quiz-answer-btn">Salir</button>
-                </div>
-            </div>
-            <div className="quiz-question-container">
-                <h2 className="quiz-question">How Would one say goodbye in Spanish?</h2>
-               <div className="quiz-btn-container">
-                    <button className="quiz-answer-btn">Adios</button>
-                    <button className="quiz-answer-btn">Hola</button>
-                    <button className="quiz-answer-btn">Au Revoir</button>
-                    <button className="quiz-answer-btn">Salir</button>
+                    <button className="quiz-answer-btn"></button>
+                    <button className="quiz-answer-btn"></button>
+                    <button className="quiz-answer-btn"></button>
+                    <button className="quiz-answer-btn"></button>
                 </div>
             </div>
             <button className="check-answers-btn">Check Answers</button>
