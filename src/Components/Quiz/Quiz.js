1fad01040bc19b8a00ed3921861e67efa3527c3d
Marlon Leon <ghostveene@gmail.com>
2023-03-15 10:45:26 -0400

    retrieving data from api still not sure how to add to page

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -1,6 +1,12 @@
 import "./Quiz.css"
 
 export function Quiz() {
+    //Need to create test with data from api
+
+    fetch("https://opentdb.com/api.php?amount=5")
+        .then(res => res.json())
+        .then(data => console.log(data))
+    
     return (
         <div className="quiz-container">
             <div className="quiz-question-container">
