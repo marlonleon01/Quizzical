20dee37376a92602490ab7457d7f519756398500
Marlon Leon <ghostveene@gmail.com>
2023-03-15 10:59:04 -0400

    removed fetch added to app.js

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -3,9 +3,6 @@ import "./Quiz.css"
 export function Quiz() {
     //Need to create test with data from api
 
-    fetch("https://opentdb.com/api.php?amount=5")
-        .then(res => res.json())
-        .then(data => console.log(data))
     
     return (
         <div className="quiz-container">
