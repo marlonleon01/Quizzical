593aa48d07df6c83c06b81690bc8d565e306d9a2
Marlon Leon <ghostveene@gmail.com>
2023-04-05 12:15:26 -0400

    removed questions state since I will be passing it with context directly to quiz component

--- src/App.js
+++ src/App.js
@@ -6,16 +6,14 @@ import blob1 from "./images/blobs.png"
 import blob2 from "./images/blob 5.png"
 
 export function App() {
-    const {startQuiz, questions, getQuiz} = useContext(Context)
+    const {startQuiz, getQuiz} = useContext(Context)
 
     return (
         <main>
             <img src={blob1} className="blob1"></img>
             {
                 startQuiz ? 
-                <Quiz 
-                    questions={questions}
-                /> : 
+                <Quiz /> : 
                 <Homescreen 
                     handleClick={getQuiz}
                 />
