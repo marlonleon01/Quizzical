942471ce07b087b0d1d2596238e3bf03a031f983
Marlon Leon <ghostveene@gmail.com>
2023-04-05 12:12:36 -0400

    moved all functionality to context

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
-import {useEffect, useState} from "react"
+import {useEffect, useState, useContext} from "react"
 import { Homescreen } from "./Components/Homescreen/Homescreen"
 import { Quiz } from "./Components/Quiz/Quiz"
+import { Context } from "./Context"
 import blob1 from "./images/blobs.png"
 import blob2 from "./images/blob 5.png"
 
 export function App() {
-    //make start button do something
-    const [startQuiz, setStartQuiz] = useState(false)
-    const [questions, setQuestions] = useState([])
-
-    function getQuiz() {
-        setStartQuiz(oldStartQuiz => !oldStartQuiz)
-    }
-
-    useEffect(() => {
-        if(startQuiz) {
-            fetch("https://opentdb.com/api.php?amount=5")
-                .then(res => res.json())
-                .then(data => setQuestions(data.results.map(data => {
-                    return({
-                        question:data.question,
-                        options:data.incorrect_answers,
-                        selectAnswer: undefined,
-                        correctAnswer: data.correct_answer
-                    })
-                })))
-    
-        }
-    },[startQuiz])
-    //after fetch return quiz with props filled
+    const {startQuiz, questions, getQuiz} = useContext(Context)
 
     return (
         <main>
