3116f0fdbd091568f9249281057d0bbf31c04e33
Marlon Leon <ghostveene@gmail.com>
2023-04-05 12:12:47 -0400

    moved functionality that was in app to context now I need to fetch data to display

---
+++ src/Context.js
@@ -0,0 +1,32 @@
+import {createContext, useState, useEffect} from "react"
+
+const Context = createContext()
+
+function ContextProvider(props) {
+    const [startQuiz, setStartQuiz] = useState(false)
+    const [questions, setQuestions] = useState([])
+
+    function getQuiz() {
+        setStartQuiz(oldStartQuiz => !oldStartQuiz)
+    }
+
+    useEffect(() => {
+        if(startQuiz) {
+            fetch("https://opentdb.com/api.php?amount=5")
+                .then(res => res.json())
+                .then(data => console.log(data))
+        }
+    },[startQuiz])
+    
+    return (
+        <Context.Provider value={{
+            startQuiz,
+            questions,
+            getQuiz
+        }}>
+            {props.children}
+        </Context.Provider>
+    )
+}
+
+export {Context, ContextProvider}
