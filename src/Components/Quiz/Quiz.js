dad0f38ed2cdd0adaf801ebed2e10f8266bc9e98
Marlon Leon <ghostveene@gmail.com>
2023-04-05 15:28:09 -0400

    rendering questions and answers at random

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -1,18 +1,38 @@
 import "./Quiz.css"
+import { useContext, useEffect } from "react"
+import { Context } from "../../Context"
 
 export function Quiz() {
+const {startQuiz, questions} = useContext(Context)
+
+function renderAnswers(question) {
+    const incorrectAnswers = question.incorrectAnswer
+    const randomIndex = Math.floor(Math.random() * (incorrectAnswers.length + 1))
+    incorrectAnswers.splice(randomIndex, 0, question.correctAnswer)
+
+    return incorrectAnswers.map((answer, answerIndex) => (
+        <button key={answerIndex} className="quiz-answer-btn">
+            {answer}
+        </button>
+    ))
+}
+
     return (
         <div className="quiz-container">
-            <div className="quiz-question-container">
-                <h2 className="quiz-question"></h2>
-                <div className="quiz-btn-container">
-                    <button className="quiz-answer-btn"></button>
-                    <button className="quiz-answer-btn"></button>
-                    <button className="quiz-answer-btn"></button>
-                    <button className="quiz-answer-btn"></button>
-                </div>
-            </div>
-            <button className="check-answers-btn">Check Answers</button>
+            {startQuiz && questions.length > 0 ? (
+                <>
+                    {questions.map((question, questionIndex) => (
+                        <div key={questionIndex} className="quiz-question-container">
+                            <h2 className="quiz-question">{question.question}</h2>
+                            <div className="quiz-btn-container">
+                                {renderAnswers(question)}
+                            </div>
+                        </div>
+                    ))}
+                    <button className="check-answers-btn">Check Answers</button>
+                </>
+            ) : <div></div>
+        }
         </div>
     )
 }
